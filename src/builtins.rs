7f308bd88e9cffe359c5f5c76b6c2a8ed4ccf3da
Flakebi <flakebi@t-online.de>
2019-01-25 18:34:28 +0100

    Implement paging for list

--- src/builtins.rs
+++ src/builtins.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::sync::Weak;
 
 use futures::{future, Future};
-use parking_lot::RwLock;
+use parking_lot::{Mutex, RwLock};
 use regex::Regex;
 use slog::{debug, error, info};
 use tsclientlib::ConnectionLock;
@@ -19,8 +19,9 @@ pub fn init(b2: Weak<RwLock<Bot>>, bot: &mut Bot) {
 	let help_regex = Regex::new(&format!("^{}help", p)).unwrap();
 	add_fun(bot, help_regex, move |b, _, _| help(b));
 
+	let list_mutex = Mutex::new(None);
 	let list_regex = Regex::new(&format!("^{}list", p)).unwrap();
-	add_fun(bot, list_regex, move |b, _, _| list(b));
+	add_fun(bot, list_regex, move |b, _, m| list(b, &list_mutex, m));
 
 	let add_regex = Regex::new(&format!("^{}add",
 		p)).unwrap();
@@ -188,36 +189,82 @@ fn quit<'a>(bot: &Bot, con: &ConnectionLock, msg: &'a Message) -> Option<Cow<'a,
 /// Please do not remove the help message.
 fn help<'a>(bot: &Bot) -> Option<Cow<'a, str>> {
 	Some(format!("This is a [URL=https://github.com/ReSpeak/SimpleBot]SimpleBot[/URL].\n\
-		Use '{prefix}add <reaction> on <trigger>' to add new actions\n\
-		or '{prefix}del <trigger>' to remove them.\n\
-		'{prefix}list' lists all commands and actions.\n\
-		'{prefix}quit' disconnects the bot.", prefix = bot.settings.prefix)
+		Use [i]{prefix}add <reaction> on <trigger>[/i] to add new actions\n\
+		or [i]{prefix}del <trigger>[/i] to remove them.\n\
+		[i]{prefix}list[/i] lists all commands and actions.\n\
+		[i]{prefix}quit[/i] disconnects the bot.", prefix = bot.settings.prefix)
 		.into())
 }
 
-fn list<'a>(bot: &Bot) -> Option<Cow<'a, str>> {
-	let mut matchers = Vec::new();
-	for a in &bot.actions.0 {
-		let mut res = String::new();
-		for m in &a.matchers {
-			match m {
-				Matcher::Regex(r) => {
-					let mut r = r.as_str().to_string();
-					r = r.replace(&['^', '$'][..], "");
-					r = r.replace("\\b", "");
+type ListPages = Vec<String>;
+fn list<'a>(bot: &Bot, list: &Mutex<Option<ListPages>>, msg: &Message)
+	-> Option<Cow<'a, str>> {
+	let mut list = list.lock();
+	if list.is_none() {
+		let mut matchers = Vec::new();
+		for a in &bot.actions.0 {
+			let mut res = String::new();
+			for m in &a.matchers {
+				match m {
+					Matcher::Regex(r) => {
+						let mut r = r.as_str().to_string();
+						r = r.replace(&['^', '$'][..], "");
+						r = r.replace("\\b", "");
 
-					r = r.replace("\\.", ".");
-					res.push_str(&r);
+						r = r.replace("\\.", ".");
+						res.push_str(&r);
+					}
+					Matcher::Mode(m) => res.push_str(&format!(" (only in {} mode)",
+						Reaction::get_mode(m))),
 				}
-				Matcher::Mode(m) => res.push_str(&format!(" (only in {} mode)",
-					Reaction::get_mode(m))),
+			}
+			matchers.push(res);
+		}
+		matchers.sort_unstable();
+		matchers.dedup();
+
+		// Group lines so thet at maximum 900 chars are on one page
+		// (there will be additional text later).
+		let mut res = vec![String::new()];
+		for m in matchers {
+			if res.last().unwrap().len() + m.len() > 900 {
+				res.push(String::new());
+			}
+			let cur = res.last_mut().unwrap();
+			cur.push('\n');
+			cur.push_str(&m);
+		}
+
+		*list = Some(res);
+	}
+
+	let list = list.as_ref().unwrap();
+
+	let mut page = 0;
+	if let Some(i) = msg.message.rfind(' ') {
+		if let Ok(n) = (msg.message[i+1..]).parse::<usize>() {
+			if n != 0 {
+				// Start indexing at 1
+				page = n - 1;
 			}
 		}
-		matchers.push(res);
 	}
-	matchers.sort_unstable();
-	matchers.dedup();
-	let res = format!("\n{}", matchers.join("\n"));
+
+	if page >= list.len() {
+		page = list.len() - 1;
+	}
+
+	let page_s = list[page].clone();
+	let res = if list.len() > 1 {
+		format!("Page {}/{}, use [i]{}list <page>[/i] to show more.{}",
+			page + 1,
+			list.len(),
+			bot.settings.prefix,
+			page_s,
+		)
+	} else {
+		page_s
+	};
 
 	Some(res.into())
 }
